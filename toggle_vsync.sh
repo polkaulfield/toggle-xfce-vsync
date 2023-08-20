ef9c76e4ad1a6ea88e509ff994044eecfd3b4b66
polkaulfield <mr.tamuraa@gmail.com>
2023-08-20 13:45:04 +0200

    Fixed a dumb mistake

--- toggle_vsync.sh
+++ toggle_vsync.sh
@@ -3,9 +3,6 @@
 # Place your vsync backend here (xpresent for amd, glx nvidia usually)
 BACKEND=xpresent
 
-CURRENT_BACKEND=$(xfconf-query -c xfwm4 -p /general/vblank_mode)
-
-
 if [ $CURRENT_BACKEND == off ]; then
 	xfconf-query -c xfwm4 -p /general/vblank_mode -s $BACKEND
 	xfwm4 --replace --vblank $BACKEND &
